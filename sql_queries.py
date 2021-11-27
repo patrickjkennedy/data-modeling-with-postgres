9490bca333bfba7b824baa0f6ff536c857b1b128
patrickjkennedy <patrick.kennedy@phorest.com>
2021-11-27 22:06:53 +0000

    Finished etl.py.

--- sql_queries.py
+++ sql_queries.py
@@ -68,23 +68,27 @@ VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
 
 user_table_insert = ("""
 INSERT INTO users (userId, firstName, lastName, gender, level)
-VALUES (%s, %s, %s, %s, %s);
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (userId) DO NOTHING;
 """)
 
 song_table_insert = ("""
 INSERT INTO songs (song_id, title, artist_id, year, duration)
-VALUES (%s, %s, %s, %s, %s);
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (song_id) DO NOTHING;
 """)
 
 artist_table_insert = ("""
 INSERT INTO artists (artist_id, artist_name, artist_location, artist_latitude, artist_longitude)
-VALUES (%s, %s, %s, %s, %s);
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (artist_id) DO NOTHING;
 """)
 
 
 time_table_insert = ("""
 INSERT INTO time (start_time, hour, day, week, month, year, weekday)
-VALUES (%s, %s, %s, %s, %s, %s, %s);
+VALUES (%s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT (start_time) DO NOTHING;
 """)
 
 # FIND SONGS
